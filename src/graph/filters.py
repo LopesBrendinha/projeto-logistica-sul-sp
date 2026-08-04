bc463a5f530071f157c10807254675297fed148f
LopesBrendinha <brendalopes.unicentro@gmail.com>
2026-08-04 16:18:23 -0300

    feat: implementa filtro de nós e adjacências por estado

---
+++ src/graph/filters.py
@@ -0,0 +1,41 @@
+ESTADOS = {"PR", "SC", "RS", "SP"}
+
+
+def filtrar_nos(nos):
+    """
+    Mantém apenas os nós dos estados desejados.
+    """
+
+    nos_filtrados = {}
+
+    for id_no, info in nos.items():
+
+        if info["uf_principal"] in ESTADOS:
+            nos_filtrados[id_no] = info
+
+    return nos_filtrados
+
+def filtrar_adjacencia(adjacencia, nos_filtrados):
+    """
+    Mantém apenas as conexões entre nós filtrados.
+    """
+
+    ids_validos = set(nos_filtrados.keys())
+
+    nova_adjacencia = {}
+
+    for origem, vizinhos in adjacencia.items():
+
+        if origem not in ids_validos:
+            continue
+
+        nova_adjacencia[origem] = []
+
+        for aresta in vizinhos:
+
+            destino = str(aresta["destino"])
+
+            if destino in ids_validos:
+                nova_adjacencia[origem].append(aresta)
+
+    return nova_adjacencia
