ba4a41ef5a6d34e149674207f686be41ff23f1a0
LopesBrendinha <brendalopes.unicentro@gmail.com>
2026-08-04 16:18:58 -0300

    feat: implementa busca de componentes conectados com DFS

--- src/graph/components.py
+++ src/graph/components.py
@@ -1,1 +1,41 @@
-    
+def encontrar_componentes(adjacencia):
+    """
+    Encontra todos os componentes conectados do grafo utilizando DFS.
+
+    Parâmetros:
+        adjacencia (dict): Lista de adjacência do grafo.
+
+    Retorna:
+        list: Lista de componentes, onde cada componente é uma lista de nós.
+    """
+
+    visitados = set()
+    componentes = []
+
+    for no in adjacencia:
+
+        if no not in visitados:
+
+            componente = []
+            pilha = [no]
+
+            while pilha:
+
+                atual = pilha.pop()
+
+                if atual in visitados:
+                    continue
+
+                visitados.add(atual)
+                componente.append(atual)
+
+                for aresta in adjacencia[atual]:
+
+                    destino = str(aresta["destino"])
+
+                    if destino not in visitados:
+                        pilha.append(destino)
+
+            componentes.append(componente)
+
+    return componentes
